765a3212369cfedc44f853835e8d25698bbdfa84
Fernando Zamora <fernandozamoraj@hotmail.com>
2017-05-05 00:53:57 -0500

    added validation for login... removed some files for cleanup... changed av1.PNG

---
+++ Blip/Controllers/BlipController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blip.Models;
+
+namespace Blip.Controllers
+{
+    public class BlipController : Controller
+    {
+        // GET: BlipUser
+        public ActionResult Index()
+        {
+            BlipUserCreateViewModel model = new BlipUserCreateViewModel
+            {
+                Avatars = BlipRepo.Current.GetAvatars(),
+                SelectedAvatar = "",
+                User = new BlipUser()
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Users()
+        {
+            return View(BlipRepo.Current.GetAll());
+        }
+
+    }
+}
